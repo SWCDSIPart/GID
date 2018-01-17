b0f0ac746a62827353b7065a52e7db775181d096
YW Sung <sungyw@gmail.com>
2018-01-17 15:54:25 +0900

    fabric-sample 제거

--- gid_rest/fabric-samples/fabcar/joinPeer1.sh
+++
@@ -1,1 +0,0 @@
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_ADDRESS=10.113.58.207:7051" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
